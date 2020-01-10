7529f75f894db9ec9e447e2fecca4efe05b7a3ae
Gabriel Galan <gabrielgalan@hotmail.com>
2020-01-09 23:16:53 -0600

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask import jsonify
+from api.utils.database import db
+
+def create_app(config):
+    app = Flask(__name__)
+
+    app.config.from_object(config)
+    
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    return app
+
+if os.environ.get('WORK_ENV') == 'PROD':
+    app_config = ProductionConfig
+elif os.environ.get('WORK_ENV') == 'TEST':
+    app_config = TestingConfig
+else:
+    app_config = DevelopmentConfig
+
+app.config.from_object(app_config)
+
+if __name__ == '__main__':
+    app.run(port=5000, hots='0.0.0.0', use_reloader=False)
